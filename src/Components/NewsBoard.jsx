e9c6ac5e12537752c166028835f4df20365690d6
Prathyu048 <bprathyu04@gmail.com>
2025-04-11 00:18:06 +0530

    add localhost as origin for api call

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,12 +6,20 @@ const NewsBoard = ({ category, searchQuery }) => {
 
   useEffect(() => {
     const fetchArticles = async () => {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-      const response = await fetch(url);
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Origin': 'http://localhost:5173', // or whatever port your local dev server runs on
+          'Referer': 'http://localhost:5173'
+        }
+      });
+    
       const data = await response.json();
       setArticles(data.articles);
     };
-
+    
     fetchArticles();
   }, [category]);
 
